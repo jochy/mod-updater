781c2195b4b4a67b2ead0cc1d279e67fa5c73aaf
Joch <joch41@gmail.com>
2022-10-29 17:31:55 +0200

    Resolves #1

--- website/src/components/mods/GroupMods.vue
+++ website/src/components/mods/GroupMods.vue
@@ -9,50 +9,11 @@
         }}
       </el-button>
     </div>
-    <div v-if="followedGroups != null && followedGroups.length > 0 && hide != null">
-      <el-card class="box-card" v-for="group in followedGroups" :key="group.id" shadow="never"
-               :body-style="hide[group.id] == null ? '' : hide[group.id] ? '' : 'display:none'">
-        <template #header>
-          <div class="card-header">
-            <span>{{ group.title }}</span>
-            <div>
-              <el-button-group>
-                <el-button type="danger" @click="askRemove($event, group.id)" plain>
-                  <i class="fa-duotone fa-trash-can"></i>
-                </el-button>
-                <el-button v-if="authenticatedUser != null && group.creator === authenticatedUser.uid" type="warning"
-                           plain
-                           @click="() => {this.toEdit = group; this.showEditDialog = true;}">
-                  <i class="fa-duotone fa-pen-to-square"></i>
-                </el-button>
-                <el-button type="info"
-                           @click="hideGroup($event, group.id)"
-                           plain>
-                  <i class="fa-duotone fa-eye-slash"></i>
-                </el-button>
-              </el-button-group>
-            </div>
-          </div>
-        </template>
-        <div style="text-align: left">
-          <div style="width: calc(50% - 5px);display: inline-block;text-align:left;vertical-align: top;padding:5px">
-            {{
-              group.desc
-            }}
-          </div>
-          <div
-              style="display: inline-block;padding:5px;width: calc(50% - 20px);border-left: 1px solid var(--el-border-color)">
-            <p v-for="mod in group.mods" :key="mod.id" class="text">
-              <el-avatar shape="square" :size="50" :src="mod.preview"/>
-              <span style="line-height: 50px; vertical-align: text-bottom; margin-left: 5px">
-              <router-link :to="{ name: 'detail', params: { id: mod.id }}">
-                  <el-button link type="primary" @click="">{{ mod.name }}</el-button>
-                </router-link>
-            </span>
-            </p>
-          </div>
-        </div>
-      </el-card>
+    <div v-if="followedGroups != null && followedGroups.length > 0">
+      <mod-group-item :group="item" v-for="item in followedGroups" :key="item.id"
+                      @edit-group="group => {this.toEdit = group; this.showEditDialog = true;}"
+                      @remove-group="group => this.askRemove(group)"
+      />
     </div>
 
     <div v-if="followedGroups.length === 0">
@@ -75,20 +36,20 @@
 
 <script>
 import NewGroupDialog from "./NewGroupDialog.vue";
+import ModGroupItem from './ModGroupItem.vue';
 import {mapGetters, mapMutations} from "vuex";
 import {ElMessage, ElMessageBox} from "element-plus";
 
 export default {
   name: "GroupMods",
   components: {
-    NewGroupDialog
+    NewGroupDialog, ModGroupItem
   },
   data: function () {
     return {
       showCreateDialog: false,
       showEditDialog: false,
       toEdit: null,
-      hide: {}
     };
   },
   computed: {
@@ -115,8 +76,7 @@ export default {
         this.showCreateDialog = true;
       }
     },
-    askRemove: function ($event, id) {
-      this.blur($event);
+    askRemove: function (group) {
       ElMessageBox.confirm(
           this.$t(`message.mods.group_remove_dialog.desc`),
           this.$t(`message.mods.group_remove_dialog.title`),
@@ -126,7 +86,7 @@ export default {
             type: 'error',
           }
       )
-          .then(() => window.ipcRenderer.send('group-removed', {id: id}))
+          .then(() => window.ipcRenderer.send('group-removed', {id: group.id}))
           .catch(() => console.log('Deleted canceled'));
     },
     displayHelp: function () {
@@ -141,10 +101,6 @@ export default {
             draggable: true
           }
       )
-    },
-    hideGroup: function ($event, gid) {
-      this.blur($event);
-      this.hide[gid] = this.hide[gid] != null ? !this.hide[gid] : false
     },
     groupValidated: function (group) {
       this.showCreateDialog = false;
